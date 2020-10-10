2db3ef13b8cddd43ff5ff86223a5c128ecf26f4d
dalpaeng00 <dalpaeng00@naver.com>
2020-10-10 22:47:19 +0900

    Updates

---
+++ precache-manifest.58a00c732c8a3e057da196556a8d4b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "114d3434d006dc29083b",
+    "url": "/movie_app_2020/static/js/runtime~main.6c3fa8d1.js"
+  },
+  {
+    "revision": "a7e5f3ef1648749fd71c",
+    "url": "/movie_app_2020/static/js/main.0e2b9d41.chunk.js"
+  },
+  {
+    "revision": "edf86c7361e817ee764b",
+    "url": "/movie_app_2020/static/js/2.49b9be94.chunk.js"
+  },
+  {
+    "revision": "a7e5f3ef1648749fd71c",
+    "url": "/movie_app_2020/static/css/main.fb506d62.chunk.css"
+  },
+  {
+    "revision": "b83396581915bab0d421e7bd1334716e",
+    "url": "/movie_app_2020/index.html"
+  }
+];
